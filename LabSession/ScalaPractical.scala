c217d8a2bcd7537bc880df867bac07ecf3d82d6a
D.N.Kodithuwakku <140165465+DilshaniNK@users.noreply.github.com>
2024-06-16 01:38:35 +0530

    Add files via upload

---
+++ LabSession/ScalaPractical.scala
@@ -0,0 +1,16 @@
+
+
+//Question01
+def area_of_disk(r: Double)=math.Pi*r*r;
+//Question02
+def convertTemperature(c:Double)=c*1.8000+32.00;
+//Question03
+def volume_of_sphere(r: Double)=(4*math.Pi*r*r*r)/3;
+//Question04
+def cost()=24.95-(24.95*0.4) + 3+10*0.75;
+//question05
+def  running_time()=2*8+3*7+2*8;
+
+
+
+
