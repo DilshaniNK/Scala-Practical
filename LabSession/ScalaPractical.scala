7b86653ed8f8bd5a38698b13b67c6198a5cf8608
D.N.Kodithuwakku <140165465+DilshaniNK@users.noreply.github.com>
2024-06-17 20:42:33 +0530

    Update ScalaPractical.scala

--- LabSession/ScalaPractical.scala
+++ LabSession/ScalaPractical.scala
@@ -1,5 +1,7 @@
 
 
+
+
 //Question01
 def area_of_disk(r: Double):Double=math.Pi*r*r;
 //Question02
@@ -10,10 +12,10 @@ def volume_of_sphere(r: Double):Double=(4*math.Pi*r*r*r)/3;
 def calculate_dis():Double=24.95*0.4;
 def calculate_cost(x: Int):Double={
     if(x > 50){
-        24.95-calculate_dis()+3+(x-50)*0.75;
+        (24.95-calculate_dis())*x+3+(x-50)*0.75;
     }
     else{
-        24.95-calculate_dis()+3;
+        (24.95-calculate_dis())*x+3;
     }
 }
 //question05
