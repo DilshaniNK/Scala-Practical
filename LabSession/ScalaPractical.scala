f77785a3bb10cf0d13922517bc006de11a172854
D.N.Kodithuwakku <140165465+DilshaniNK@users.noreply.github.com>
2024-06-17 20:17:20 +0530

    Update ScalaPractical.scala

--- LabSession/ScalaPractical.scala
+++ LabSession/ScalaPractical.scala
@@ -1,15 +1,24 @@
 
 
 //Question01
-def area_of_disk(r: Double)=math.Pi*r*r;
+def area_of_disk(r: Double):Double=math.Pi*r*r;
 //Question02
-def convertTemperature(c:Double)=c*1.8000+32.00;
+def convertTemperature(c:Double):Double=c*1.8000+32.00;
 //Question03
-def volume_of_sphere(r: Double)=(4*math.Pi*r*r*r)/3;
+def volume_of_sphere(r: Double):Double=(4*math.Pi*r*r*r)/3;
 //Question04
-def cost()=24.95-(24.95*0.4) + 3+10*0.75;
+def calculate_dis():Double=24.95*0.4;
+def calculate_cost(x: Int):Double={
+    if(x > 50){
+        24.95-calculate_dis()+3+(x-50)*0.75;
+    }
+    else{
+        24.95-calculate_dis()+3;
+    }
+}
 //question05
-def  running_time()=2*8+3*7+2*8;
+def  easyplace(x: Int):Double=8*x;
+def tempo(y: Int):Double=7*y;
 
 
 
